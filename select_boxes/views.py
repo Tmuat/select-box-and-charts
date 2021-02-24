980bbe201b325b76f15f6387e1f058d3e4859d69
Tmuat <Thomas_Muat@hotmail.com>
2021-02-24 14:04:34 +0000

    Created function and JS to delete selected checkboxes

--- select_boxes/views.py
+++ select_boxes/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from select_boxes.models import Items
 
 
 def select(request):
 
+    if request.method == 'POST':
+        selected = request.POST.get('id-selected')
+        ids_split = selected.split(",")
+        for id in ids_split:
+            Items.objects.filter(id=id).delete()
+
+    qs = Items.objects.all()
+
     context = {
+        'items': qs
     }
 
     template = 'select.html'
